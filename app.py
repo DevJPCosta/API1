8719341628f0107a6f6b396c5b6cf477b3b24444
M0narca <jppcsantosescola@gmail.com>
2023-04-18 16:50:06 -0300

    third commit

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ config = {
     "apiKey": "AIzaSyAQsgygjK06AsyrAKg_HrbvO4dzItFeYTU",
     "authDomain": "conecaooporapi.firebaseapp.com",
     "databaseURL": "https://conecaooporapi-default-rtdb.firebaseio.com",
-    "projectId ": "conecaooporapi",
+    "projectId": "conecaooporapi",
     "storageBucket": "conecaooporapi.appspot.com",
     "messagingSenderId": "467005917293",
-    "appId": "1:467005917293:web:1aa02d35bb9483ae8b976"
+    "appId": "1:467005917293:web:1aa02d35bb9483ae8b9764"
 }
 firebase = pyrebase.initialize_app(config)
 
@@ -19,50 +19,51 @@ app = Flask(__name__)
 
 livros = [
     {
-        'id': 1,
-        'título': 'O Senhor dos Anéis - A Sociedade do Anel',
-        'autor': 'J.R.R Tolkien'
+        "id": 1,
+        "título": "O Senhor dos Anéis - A Sociedade do Anel",
+        "autor": "J.R.R Tolkien"
     },
     {
-        'id': 2,
-        'titulo': 'Harry Potter e a Pedra Filosofal',
-        'autor': 'J.K Howling'
+        "id": 2,
+        "titulo": "Harry Potter e a Pedra Filosofal",
+        "autor": "J.K Howling"
     },
     {
-        'id': 3,
-        'titulo': 'James Clear',
-        'autor': 'Hábitos Atômicos'
+        "id": 3,
+        "titulo": "James Clear",
+        "autor": "Hábitos Atômicos"
     },
 ]
 
 # Consultar (todos)
 
 
-@app.route('/livros', methods=['GET'])
+@app.route("/livros", methods=["GET"])
 def obter_livros():
     return jsonify(livros)
+
 # Consultar (id)
 
 
-@app.route('/livros/<int:id>', methods=['GET'])
+@app.route("/livros/<int:id>", methods=["GET"])
 def obter_livro_por_id(id):
     for livro in livros:
-        if livro.get('id') == id:
+        if livro.get("id") == id:
             return jsonify(livro)
 
 
 # Editar
-@app.route('/livros/<int:id>', methods=['PUT'])
+@app.route("/livros/<int:id>", methods=["PUT"])
 def editar_livro_por_id(id):
     livro_alterado = request.get_json()
     for indice, livro in enumerate(livros):
-        if livro.get('id') == id:
+        if livro.get("id") == id:
             livros[indice].update(livro_alterado)
             return jsonify(livros[indice])
 
 
 # Criar
-@app.route('/livros', methods=['POST'])
+@app.route("/livros", methods=["POST"])
 def incluir_novo_livro():
     novo_livro = request.get_json()
     livros.append(novo_livro)
@@ -71,10 +72,10 @@ def incluir_novo_livro():
 
 
 # Excluir
-@app.route('/livros/<int:id>', methods=['DELETE'])
+@app.route("/livros/<int:id>", methods=["DELETE"])
 def excluir_livro(id):
     for indice, livro in enumerate(livros):
-        if livro.get('id') == id:
+        if livro.get("id") == id:
             del livros[indice]
 
     return jsonify(livros)
--- app2.py
+++ app2.py
@@ -1,14 +1,20 @@
 import pyrebase
+import firebase_admin
+from firebase_admin import credentials
+
+cred = credentials.Certificate("path/to/serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
 
 config = {
     "apiKey": "AIzaSyAQsgygjK06AsyrAKg_HrbvO4dzItFeYTU",
     "authDomain": "conecaooporapi.firebaseapp.com",
     "databaseURL": "https://conecaooporapi-default-rtdb.firebaseio.com",
-    "projectId ": "conecaooporapi",
+    "projectId": "conecaooporapi",
     "storageBucket": "conecaooporapi.appspot.com",
     "messagingSenderId": "467005917293",
     "appId": "1:467005917293:web:1aa02d35bb9483ae8b976"
 }
+
 firebase = pyrebase.initialize_app(config)
 
 storage = firebase.storage()
@@ -17,4 +23,4 @@ storage = firebase.storage()
 
 storage.child("images/new.jpg").download("example.jpg")
 
-printf(storage.child("images/new.jpg").get_url(None))
+print(storage.child("images/new.jpg").get_url(None))
