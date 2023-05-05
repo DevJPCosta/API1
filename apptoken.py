7bee4c9ed8e00df0fad57f5f2c89e9fdcba9ceb4
M0narca <jppcsantosescola@gmail.com>
2023-05-05 16:39:08 -0300

    ooop

---
+++ apptoken.py
@@ -0,0 +1,12 @@
+
+
+
+POST /token HTTP/1.1
+Host: oauth2.googleapis.com
+Content-Type: application/x-www-form-urlencoded
+
+code=4/P7q7W91a-oMsCeLvIaQm6bTrgtp7&
+client_id=115864612534871729740
+client_secret=your_client_secret&
+redirect_uri=http://127.0.0.1:9004&
+grant_type=authorization_code
